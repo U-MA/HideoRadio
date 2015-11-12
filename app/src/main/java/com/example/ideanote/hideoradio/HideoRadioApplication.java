9c6556b665092ee22f744c061be620ca68765cf7
U-MA <g.0224incident@gmail.com>
2015-11-12 19:47:07 +0900

    Add Deploygate

--- app/src/main/java/com/example/ideanote/hideoradio/HideoRadioApplication.java
+++ app/src/main/java/com/example/ideanote/hideoradio/HideoRadioApplication.java
@@ -4,11 +4,13 @@ import android.app.Application;
 import android.content.res.Configuration;
 
 import com.activeandroid.ActiveAndroid;
+import com.deploygate.sdk.DeployGate;
 
 public class HideoRadioApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         ActiveAndroid.initialize(this);
+        DeployGate.install(this);
     }
 }
