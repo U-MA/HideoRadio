27242bd8595997454a9f9bacc724fd0da00ef523
U-MA <g.0224incident@gmail.com>
2015-11-27 00:38:50 +0900

    Fix Message

--- app/src/main/java/com/example/ideanote/hideoradio/dialog/ClearCacheDialog.java
+++ app/src/main/java/com/example/ideanote/hideoradio/dialog/ClearCacheDialog.java
@@ -16,7 +16,6 @@ public class ClearCacheDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Delete downloaded file")
-               .setMessage("XXX")
                .setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
                    @Override
                    public void onClick(DialogInterface dialog, int which) {
@@ -27,6 +26,9 @@ public class ClearCacheDialog extends DialogFragment {
                    }
 
                });
+        if (episode != null) {
+            builder.setMessage(episode.getTitle());
+        }
         return builder.create();
     }
 
