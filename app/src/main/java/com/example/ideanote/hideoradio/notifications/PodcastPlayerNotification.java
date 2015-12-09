a7fcc997eabf9ec0bf05c884bfe3b43378e3d9fa
U-MA <g.0224incident@gmail.com>
2015-12-10 01:33:02 +0900

    Add actions to Notification
    Notificationから再生・一時停止・停止をいじれるようになった

--- app/src/main/java/com/example/ideanote/hideoradio/notifications/PodcastPlayerNotification.java
+++ app/src/main/java/com/example/ideanote/hideoradio/notifications/PodcastPlayerNotification.java
@@ -4,13 +4,16 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.example.ideanote.hideoradio.Episode;
+import com.example.ideanote.hideoradio.PodcastPlayer;
 import com.example.ideanote.hideoradio.R;
 import com.example.ideanote.hideoradio.activities.MainActivity;
+import com.example.ideanote.hideoradio.services.PodcastPlayerService;
 
 public class PodcastPlayerNotification {
     private final static int PLAYBACK_NOTIFICATION_ID = 1000;
@@ -43,11 +46,23 @@ public class PodcastPlayerNotification {
         manager.cancel(PLAYBACK_NOTIFICATION_ID);
     }
 
+    private static PendingIntent createPauseIntent(Context context, Episode episode) {
+        Intent intent = PodcastPlayerService.createPlayPauseIntent(context, episode);
+        return PendingIntent.getService(context, 100, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
+    private static PendingIntent createStopIntent(Context context, Episode episode) {
+        Intent intent = PodcastPlayerService.createStopIntent(context, episode);
+        return PendingIntent.getService(context, 100, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
     private static Notification buildPlayNotification(Context context, Episode episode) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_action_playback_play)
                 .setContentTitle(episode.getTitle())
                 .setContentText(episode.getDescription())
+                .addAction(R.drawable.ic_action_playback_pause, "PAUSE", createPauseIntent(context, episode))
+                .addAction(R.drawable.ic_action_cancel, "STOP", createStopIntent(context, episode))
                 .setOngoing(true);
 
         PendingIntent intent = PendingIntent.getActivity(context, 0,
@@ -61,7 +76,9 @@ public class PodcastPlayerNotification {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_action_playback_pause)
                 .setContentTitle(episode.getTitle())
-                .setContentText(episode.getDescription());
+                .setContentText(episode.getDescription())
+                .addAction(R.drawable.ic_action_playback_play, "PLAY", createPauseIntent(context, episode))
+                .addAction(R.drawable.ic_action_cancel, "STOP", createStopIntent(context, episode));
 
         PendingIntent intent = PendingIntent.getActivity(context, 0,
                 MainActivity.createIntent(context, episode.getEpisodeId()), PendingIntent.FLAG_ONE_SHOT);
