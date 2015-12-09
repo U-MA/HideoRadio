fc83fa59f3e8d03d4cb01d2cdbfcc2ab13223df5
U-MA <g.0224incident@gmail.com>
2015-12-10 00:24:03 +0900

    Cancel download

---
+++ app/src/main/java/com/example/ideanote/hideoradio/dialog/EpisodeDownloadCancelDialog.java
@@ -0,0 +1,37 @@
+package com.example.ideanote.hideoradio.dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+import com.example.ideanote.hideoradio.Episode;
+import com.example.ideanote.hideoradio.events.BusHolder;
+import com.example.ideanote.hideoradio.events.EpisodeDownloadCancelEvent;
+import com.example.ideanote.hideoradio.services.EpisodeDownloadService;
+
+public class EpisodeDownloadCancelDialog extends DialogFragment {
+
+    private Episode episode;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Download cancel")
+               .setMessage("Cancel downloading?")
+               .setPositiveButton("CANCEL DOWNLOAD", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       EpisodeDownloadService.cancel(getContext(), episode);
+                   }
+               });
+        return builder.create();
+    }
+
+    public void setEpisode(Episode episode) {
+        this.episode = episode;
+    }
+}
--- app/src/main/java/com/example/ideanote/hideoradio/dialog/MediaPlayConfirmationDialog.java
+++ app/src/main/java/com/example/ideanote/hideoradio/dialog/MediaPlayConfirmationDialog.java
@@ -10,7 +10,9 @@ import com.example.ideanote.hideoradio.events.BusHolder;
 import com.example.ideanote.hideoradio.events.ClearCacheEvent;
 import com.example.ideanote.hideoradio.events.DownloadEvent;
 import com.example.ideanote.hideoradio.Episode;
+import com.example.ideanote.hideoradio.events.EpisodeDownloadCancelEvent;
 import com.example.ideanote.hideoradio.events.PlayCacheEvent;
+import com.example.ideanote.hideoradio.services.EpisodeDownloadService;
 
 /**
  * A dialog that confirm playing or downloading/streaming episode
@@ -20,12 +22,13 @@ public class MediaPlayConfirmationDialog extends DialogFragment {
     private static final String PLAY_STREAMING_TEXT      = "PLAY STREAMING";
     private static final String PLAY_CACHE_TEXT          = "PLAY";
     private static final String DOWNLOAD_TEXT            = "DOWNLOAD";
+    private static final String CANCEL_DOWNLOAD_TEXT     = "CANCEL DOWNLOAD";
     private static final String DELETE_EPISODE_FILE_TEXT = "DELETE EPISODE FILE";
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         String episodeId = getArguments().getString("episodeId");
-        Episode episode = Episode.findById(episodeId);
+        final Episode episode = Episode.findById(episodeId);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(episode.getTitle())
@@ -50,13 +53,23 @@ public class MediaPlayConfirmationDialog extends DialogFragment {
                 public void onClick(DialogInterface dialog, int which) {
                     BusHolder.getInstance().post(new PlayCacheEvent());
                 }
-            })
-                   .setNegativeButton(DOWNLOAD_TEXT, new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialog, int which) {
-                           BusHolder.getInstance().post(new DownloadEvent());
-                       }
-                   });
+            });
+
+            if (!EpisodeDownloadService.isDownloading(episodeId)) {
+                builder.setNegativeButton(DOWNLOAD_TEXT, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        BusHolder.getInstance().post(new DownloadEvent());
+                    }
+                });
+            } else {
+                builder.setNegativeButton(CANCEL_DOWNLOAD_TEXT, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EpisodeDownloadService.cancel(getContext(), episode);
+                    }
+                });
+            }
         }
 
         return builder.create();
